f26d971d90acfe5270852d66b0ae83c5044989c4
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2020-10-31 17:22:14 +0100

    Editor wip

---
+++ Client/Pages/FetchData.razor.cs
@@ -0,0 +1,25 @@
+﻿using FuzzyPDMP.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace FuzzyPDMP.Client.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject]
+        HttpClient Http { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        }
+    }
+}

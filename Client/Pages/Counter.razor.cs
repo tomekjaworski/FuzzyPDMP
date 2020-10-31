f7a5e245f08a67e8a39e0863ba2a75ce9b13c98c
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2020-10-31 16:02:57 +0100

    Editor wip

---
+++ Client/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuzzyPDMP.Client.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount += 1;
+        }
+    }
+}

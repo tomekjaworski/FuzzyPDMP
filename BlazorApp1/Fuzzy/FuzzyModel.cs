e749a868ae5b4a39a726a952ae7a9ba97f7ef302
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2020-11-23 12:53:55 +0100

    Model editor wip

---
+++ BlazorApp1/Fuzzy/FuzzyModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorApp1.Fuzzy
+{
+    public class FuzzyModel
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Board Board { get; set; }
+
+
+        public List<FuzzyRule> Rules { get; set; }
+
+
+        public FuzzyModel(Board board)
+        {
+            this.ID = Guid.NewGuid();
+            this.Rules = new List<FuzzyRule>();
+            this.Board = board;
+        }
+
+
+        public bool RemoveRule(FuzzyRule rule)
+        {
+            if (!this.Rules.Contains(rule))
+                return false;
+
+
+            this.Rules.Remove(rule);
+            //todo: aktualizacja reguł
+
+            return true;
+        }
+
+
+        public FuzzyRule AddRule()
+        {
+            FuzzyRule fr = new FuzzyRule();
+            this.Rules.Add(fr);
+            return fr;
+        }
+
+
+    }
+}
+
---
+++ BlazorApp1/Pages/Models.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Models
+    {
+        public Models()
+        {
+
+        }
+    }
+}

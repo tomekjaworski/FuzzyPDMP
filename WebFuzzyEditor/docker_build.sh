b070cc844306b66e842634b92532a18d8b9db296
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2020-12-16 12:40:58 +0100

    Docker support

---
+++ WebFuzzyEditor/docker_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -f Dockerfile -t pdmp_webfuzzyeditor ..
+
+
---
+++ WebFuzzyEditor/docker_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --rm -it -p 11000:80 -v /srv/FuzzyPDMP/WebFuzzyEditor/Data/:/app/data pdmp_webfuzzyeditor
+
+
